f4069e2e5baad9df437781ab4d0c15cd3668ed76
damla yavan <damlayvn@icloud.com>
2024-07-04 17:25:35 +0300

    Dtos classes

---
+++ MultiShop/Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/GetByIdCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Catalog.Dtos.CategoryDtos
+{
+    public class GetByIdCategoryDto
+    {
+        //Id ye göre veri getirecek
+        public string CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
